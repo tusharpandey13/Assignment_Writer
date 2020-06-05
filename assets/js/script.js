58cdceecc94bb044a3bb680aff8cde6c28903943
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-05 07:18:30 +0530

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
 window.onload = function () {
 	var fileupload = document.getElementById('file-input');
 	fileupload.onchange = function () {
-		// var reader = new FileReader();
-		// reader.readAsDataURL(fileupload.files[0]);
-		// reader.onload = function (e) {
-		// 	let urlOfImageFile = URL.createObjectURL(myImageFile);
-		// 	img = loadImage(e.target.result);
-		// 	loop();
-		// };
-		const myImageFile = fileupload.files[0];
-		let urlOfImageFile = URL.createObjectURL(myImageFile);
-		imgurl = urlOfImageFile;
-		// loop();
+		var reader = new FileReader();
+		reader.readAsDataURL(fileupload.files[0]);
+		reader.onload = function (e) {
+			let urlOfImageFile = URL.createObjectURL(myImageFile);
+			img = loadImage(e.target.result, () => {
+				loop();
+			});
+		};
 	};
 };
