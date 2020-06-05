51e6ba584d38dd9a528b50c5ecf151eef6af1fb5
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-05 07:35:04 +0530

    refactor

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,5 +1,14 @@
+let textData = `Demo`;
 let img, myFont;
 let fontssss = ['fontText', 'fontText1'];
+
+let fontIndex = 1;
+let fontsize = 0.3;
+let xaxis = 104;
+let yaxis = 81;
+let w = 619;
+let linespacing = 89;
+
 let fontText = [];
 
 tmpOffsetMap = [
@@ -15,22 +24,13 @@ tmpOffsetMap = [
 	5.11111111,
 ];
 
-settings = {
-	fontIndex: 1,
-	fontsize: 0.3,
-	linespacing: 89,
-	w: 619,
-	xaxis: 104,
-	yaxis: 81,
-};
-
 // elements of list(range(32, 126)) minus the element '96'
-let dataAvailable = Array.from(new Array(94), (x, i) => i + 32);
+let dataAvailable = Array.from(new Array(93), (x, i) => i + 32);
 dataAvailable.splice(64, 1); // remove item '96'
 
 // this function has binding in index.html
 function incrementor() {
-	settings.fontIndex = (settings.fontIndex + 1) % fontssss.length;
+	fontIndex = (fontIndex + 1) % fontssss.length;
 	// console.log(fontIndex);
 	changeFont();
 }
@@ -39,19 +39,6 @@ function textChanged(text) {
 	textData = text;
 	loop();
 }
-
-function printJSON() {
-	console.log({
-		fontIndex: settings.fontIndex,
-		xaxis: settings.xaxis,
-		yaxis: settings.yaxis,
-		fontsize: settings.fontsize,
-		w: settings.w,
-		linespacing: settings.linespacing,
-	});
-}
-
-function loadJSON() {}
 
 function preload() {
 	changeFont();
@@ -63,21 +50,20 @@ function setup() {
 	canvas = createCanvas(750, 1000);
 	canvas.parent('contributing');
 	rectMode(CORNER);
-	// noLoop();
+	noLoop();
 }
 
 function draw() {
-	clear();
 	noLoop();
 	image(img, 0, 0, width, height);
-	if (settings.linespacing) textLeading(settings.linespacing);
-	pos = createVector(settings.xaxis, settings.yaxis);
+	if (linespacing) textLeading(linespacing);
+	pos = createVector(xaxis, yaxis);
 
 	for (var i = 0; i <= textData.length; i++) {
-		if (pos.x >= settings.xaxis + settings.w || textData[i] === '\n') {
-			pos.x = settings.xaxis + Math.round(getrand(-4, 4));
+		if (pos.x >= xaxis + w || textData[i] === '\n') {
+			pos.x = xaxis + Math.round(getrand(-4, 4));
 
-			pos.y += settings.linespacing * settings.fontsize;
+			pos.y += linespacing * fontsize;
 		}
 
 		let y_offset = 0;
@@ -86,7 +72,7 @@ function draw() {
 		let randScale = getrand(0.9, 1);
 
 		if ('textImage' + textData[i] in fontText) {
-			if (settings.fontIndex < 2 && !isNaN(textData[i])) {
+			if (fontIndex < 2 && !isNaN(textData[i])) {
 				y_shift_flag = 1;
 				y_scale = 2;
 				// y_offset = y_scale * tmpOffsetMap[Number(textData[i])];
@@ -109,17 +95,16 @@ function draw() {
 					pos.x,
 					pos.y + -20 * y_shift_flag + y_offset,
 					fontText['textImage' + textData[i]].width *
-						settings.fontsize *
+						fontsize *
 						randScale,
 					fontText['textImage' + textData[i]].height *
-						settings.fontsize *
+						fontsize *
 						y_scale *
 						randScale
 				);
 				noTint();
 			}
-			pos.x +=
-				fontText['textImage' + textData[i]].width * settings.fontsize;
+			pos.x += fontText['textImage' + textData[i]].width * fontsize;
 			if (textData[i].charCodeAt(0) == 46) {
 				pos.x += 2;
 			}
@@ -127,46 +112,18 @@ function draw() {
 	}
 }
 
-function changeFont(custom_change = undefined) {
-	console.log(settings.fontIndex);
-	if (custom_change) settings.fontIndex = custom_change;
+function getrand(min, max) {
+	return (Math.random() * (max - min) + min).toFixed(4);
+}
+
+function changeFont() {
 	dataAvailable.forEach((i) => {
 		try {
+			// console.log(str(fontssss[fontIndex]) + '/' + str(i) + '_t.png');
 			fontText['textImage' + String.fromCharCode(i)] = loadImage(
-				str(fontssss[settings.fontIndex]) + '/' + str(i) + '_t.png'
+				str(fontssss[fontIndex]) + '/' + str(i) + '_t.png'
 			);
 		} catch (error) {}
 	});
 	loop();
 }
-
-function getrand(min, max) {
-	return (Math.random() * (max - min) + min).toFixed(4);
-}
-
-let textData = `              Experiment No. 1
-
-1. A) Create a Java class called Student with the following details as variables within it.
-(i) USN
-(ii) Name
-(iii) Branch
-(iv) Phone
-Write a Java program to create nStudent objects and print the USN, Name, Branch,
-and Phone of these objects with suitable headings.
-import java.util.Scanner;
-public class student {
-    String USN;
-    String Name;
-    String branch;
-    int phone;
-    void insertRecord(String reg,String name, String brnch,int ph){
-        USN=reg;
-        Name=name;
-        branch=brnch;
-        phone=ph;
-    }
-    void displayRecord(){
-        System.out.println(USN+" "+Name+" "+branch+" "+phone);
-    }
-
-`;
